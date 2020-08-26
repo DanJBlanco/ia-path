3b1c8331ac5c8933e3627089e76bc30711d42641
DJBLANCOH <djblancoh@gmail.com>
2020-08-26 12:13:21 -0500

    comparacion de busquedas lineales y binarias

--- busqueda_binaria.py
+++ busqueda_binaria.py
@@ -1,15 +1,46 @@
-obj = int(input('Num: '))
-epsilon = 0.0001
-limit_inferior = 0.0
-limit_superior = max(1.0, obj)
-resp = (limit_superior +limit_inferior) / 2
+import random
 
-while abs(resp**2 - obj) >= epsilon:
-    print(f'resp: {resp} - lim_inf: {limit_inferior} - lim_sup: {limit_superior} - **2 {resp**2}')
-    if (resp**2 < obj ):
-        limit_inferior = resp
+def busqueda_binaria(lista, comienzo, final, objetivo, int_bin=0):
+    #print(f'Buscando {objetivo} entre {lista[comienzo]} y {lista[final-1]}')
+    
+    int_bin+=1
+
+    if comienzo > final:
+        return (False, int_bin)
+    
+    medio = (comienzo+final) // 2
+
+    if lista[medio]== objetivo:
+        return (True,int_bin)
+    elif lista[medio] < objetivo:
+        return  (busqueda_binaria(lista,medio+1,final, objetivo, int_bin))
     else:
-        limit_superior = resp
-    resp = (limit_superior +limit_inferior) / 2
+        #if lista[medio] > objetivo:
+        return (busqueda_binaria(lista,comienzo,medio-1, objetivo ,int_bin))
+   
+def busqueda_lineal(lista, objetivo, int_lin=0):
+    match = False
 
-print(f'la raiz cuadrada: {resp}')
+    for elemento in lista:  # O(n)
+        int_lin+=1
+        if elemento == objetivo:
+            match = True
+            break
+    return (match, int_lin)
+    
+    
+
+if __name__ == "__main__":
+    
+    tamano_lista = int(input('Tamanho de la lista? '))
+    objetivo = int(input('Que numero deseas encontrar? '))
+
+    lista = [random.randint(0,100) for i in range(tamano_lista)]
+
+    (encontrado,int_bin) = busqueda_binaria(sorted(lista), 0, len(lista), objetivo)
+    (encontrado,int_lin) = busqueda_lineal(lista, objetivo)
+    #print(lista)
+    print(f'El elemento: {objetivo}  {"esta" if encontrado else "no esta"}')
+    print(f'interaciones lineal: {int_lin}')
+    print(f'interaciones binary: {int_bin}')
+    
